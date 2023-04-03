b7324f8cd5662e7dfdc26706229401859c8cd404
AntonMakarkin <amakarkin27@gmail.com>
2023-04-04 00:36:43 +0300

    Making header

---
+++ components/DropDownLinks.tsx
@@ -0,0 +1,5 @@
+const DropDownLinks = () => {
+    
+};
+
+export default DropDownLinks;
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,20 @@
+import Link from "next/link";
 import { FC } from "react";
 
 const NavBar:FC = () => {
     return (
-        <header>
-            NewsAgr
-            <nav></nav>
+        <header className="flex">
+            NewsAggr
+            <nav>
+                <ul className="flex">
+                    <li>
+                        <Link href="/">Breaking</Link>
+                    </li>
+                    <li>
+                        <Link href="/search">Search</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 };
